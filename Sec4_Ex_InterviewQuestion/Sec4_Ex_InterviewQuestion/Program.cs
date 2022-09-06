50d105c1d250fdff8e15e6fd4028ed2373648695
Izalith <48108297+7kFour@users.noreply.github.com>
2022-09-05 22:38:12 -0500

    first attempt at ex4 (first exercise of class)

---
+++ Sec4_Ex_InterviewQuestion/Sec4_Ex_InterviewQuestion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sec4_Ex_InterviewQuestion {
+    internal class Program {
+        static void Main(string[] args) {
+
+            char[] arr1 = { 'a', 'b', 'c', 'x' };
+            char[] arr2 = { 'z', 'y', 'i' };
+
+            char[] arr3 = { 'a', 'b', 'c', 'x' };
+            char[] arr4 = { 'z', 'y', 'x' };
+
+            Console.WriteLine(ContainCommon(arr1, arr2)); // false
+            Console.WriteLine(ContainCommon(arr3, arr4)); // true
+        }
+
+        // first attempt
+        public static bool ContainCommon(char[] arrOne, char[] arrTwo) {
+
+            for (int i = 0; i < arrOne.Length; i++) {
+                for (int j = 0; j < arrTwo.Length; j++) { 
+                    if (arrOne[i] == arrTwo[j]) {
+                        return true;
+                    }
+                }
+            }
+            return false; 
+        }
+    }
+}
